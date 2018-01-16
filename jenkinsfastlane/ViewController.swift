dbc1b8721c8c5ea16b44890e3e8b58e47ac5355f
Soriyany <soriyanykeo@soriyanys-mbp.attlocal.net>
2018-01-15 21:59:17 -0600

    Initial commit

---
+++ jenkinsfastlane/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jenkinsfastlane
+//
+//  Created by Soriyany keo on 1/15/18.
+//  Copyright © 2018 Soriyany keo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
